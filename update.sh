77740acde612d06431057079d72e53b038979fd5
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-02-11 22:43:44 +0000

    Fix update.sh once again

--- update.sh
+++ update.sh
@@ -5,6 +5,12 @@ set -e
 git submodule init
 
 for path in $(cat .submodule_paths); do
+  git submodule update "$path" &
+done;
+
+wait
+
+for path in $(cat .submodule_paths); do
   cd "$path" && git pull &
 done;
 
