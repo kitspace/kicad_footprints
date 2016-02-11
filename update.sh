b2b4a8f2b32a65567a9a0d02147b13d371eba423
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-02-11 22:38:25 +0000

    Fix init in update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 set -e
+
+git submodule init
+
 for path in $(cat .submodule_paths); do
-  git submodule update --init "$path" &
+  git submodule update "$path" &
 done;
 
 wait
