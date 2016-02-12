cd3c144339ad1f8fb30986cc70f2a7e41370a739
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-02-12 23:30:03 +0000

    Remove superflous `wait`

--- update.sh
+++ update.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
+set -e;
 
-git submodule init
+git submodule init;
 
 for path in $(cat .submodule_paths); do
   git submodule update "$path" &
 done;
 
-wait
-
-wait
-
+wait;
