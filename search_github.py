bb5769522458e15aba74509327f284d738f52efb
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-11-24 20:01:33 +0000

    Finish and do a github search for .pretty

--- search_github.py
+++ search_github.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python
-import urllib2
+from __future__ import print_function
 import json
 import math
+import subprocess
 import time
+import urllib2
+import os
 
 PAGE_SIZE = 100
 
 def query(term, page):
-    url = "https://api.github.com/search/repositories?q={}&page={}&per_page={}".format(term, page, PAGE_SIZE)
+    url = 'https://api.github.com/search/repositories?q={}&page={}&per_page={}'.format(term, page, PAGE_SIZE)
     data = urllib2.urlopen(url).read()
     return json.loads(data)
 
-
+print('searching github')
 data = query('pretty+kicad', page=1)
 items = data['items'][:]
 
 total = data['total_count']
-print(total)
 pages = int(math.ceil(total / PAGE_SIZE))
 
 for n in range(2, pages + 2):
-    time.sleep(0.1)
-    print(n)
+    time.sleep(0.01)
     data = query('pretty+kicad', page=n)
     items += data['items']
 
+print('found {} items'.format(len(items)))
+
+for i,item in enumerate(items):
+    name = item['full_name']
+    print('{}: checking {}'.format(i, name))
+    if name.endswith('.pretty') and not os.path.exists(name):
+        cmd = ['git', 'submodule', 'add', item['ssh_url'], name]
+        subprocess.call(cmd)
