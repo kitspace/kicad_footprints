6835093ce85424efb7ec672aca36d757b2a63411
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-11-25 01:21:52 +0000

    Add echo of term in search script and re-run

--- search_github.py
+++ search_github.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 from __future__ import print_function
 import json
 import math
@@ -8,14 +7,15 @@ import urllib2
 import os
 
 PAGE_SIZE = 100
+TERM = 'pretty+kicad'
 
 def query(term, page):
     url = 'https://api.github.com/search/repositories?q={}&page={}&per_page={}'.format(term, page, PAGE_SIZE)
     data = urllib2.urlopen(url).read()
     return json.loads(data)
 
-print('searching github')
-data = query('pretty+kicad', page=1)
+print('searching github for "{}"'.format(TERM))
+data = query(TERM, page=1)
 items = data['items'][:]
 
 total = data['total_count']
@@ -23,7 +23,7 @@ pages = int(math.ceil(total / PAGE_SIZE))
 
 for n in range(2, pages + 2):
     time.sleep(0.01)
-    data = query('pretty+kicad', page=n)
+    data = query(TERM, page=n)
     items += data['items']
 
 print('found {} items'.format(len(items)))
@@ -32,5 +32,5 @@ for i,item in enumerate(items):
     name = item['full_name']
     print('{}: checking {}'.format(i, name))
     if name.endswith('.pretty') and not os.path.exists(name):
-        cmd = ['git', 'submodule', 'add', item['ssh_url'], name]
+        cmd = ['git', 'submodule', 'add', item['html_url'], name]
         subprocess.call(cmd)
