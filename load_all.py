ed484955c5a02865f36de5ece052222cd11a47d8
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-01-10 14:23:40 +0000

    Add a .travis.yml to check that footprints loads

---
+++ load_all.py
@@ -0,0 +1,22 @@
+import pcbnew
+import sys
+import os
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+pretties = []
+for dirname, dirnames, filenames in os.walk('./'):
+    # don't go into any .git directories.
+    if '.git' in dirnames:
+        dirnames.remove('.git')
+
+    for filename in filenames:
+        if (not os.path.isdir(filename)) and (os.path.splitext(filename)[-1] == '.kicad_mod'):
+            pretties.append(os.path.realpath(dirname))
+            break
+
+src_plugin = pcbnew.IO_MGR.PluginFind(1)
+
+for libpath in pretties:
+    list_of_footprints = src_plugin.FootprintEnumerate(libpath)
