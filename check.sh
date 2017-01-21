5c6b715777bbfb78692936aa7b3b75411cc1ad41
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-01-21 14:49:19 +0000

    Check for failures in check and clone

--- check.sh
+++ check.sh
@@ -1,8 +1,8 @@
 echo "Checking $1"
-if cd "$1" 
+if cd "$1"
   # might be empty dir instead of proper submodule
   then if [ -f .git ]
     then git fetch --quiet && git reset --quiet --hard origin/HEAD \
-      || echo " ** Failed ** $1"
+      || echo " ** Failed ** $1" && false
   fi
 fi
--- clone.sh
+++ clone.sh
@@ -3,7 +3,7 @@ if ! git submodule update --quiet --depth 1 "$1"
 then
   echo "falling back to deep clone for $1"
   if ! git submodule update --quiet "$1"
-  then 
-      echo "** FAILED ** $1"
+  then
+      echo "** FAILED ** $1" && false
   fi
 fi
