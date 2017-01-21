e9f61ab2e68aa3aa290f84dbc87cdcdae43c1d51
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-01-21 15:27:56 +0000

    Fix return status checking in scripts

--- check.sh
+++ check.sh
@@ -2,7 +2,12 @@ echo "Checking $1"
 if cd "$1"
   # might be empty dir instead of proper submodule
   then if [ -f .git ]
-    then git fetch --quiet && git reset --quiet --hard origin/HEAD \
-      || echo " ** Failed ** $1" && false
+    then
+      git fetch --quiet && git reset --quiet --hard origin/HEAD
   fi
+fi
+
+if [ $? != 0 ]
+then
+  echo " ** Failed ** $1" && false
 fi
--- clone.sh
+++ clone.sh
@@ -2,8 +2,10 @@ echo "Cloning $1"
 if ! git submodule update --quiet --depth 1 "$1"
 then
   echo "falling back to deep clone for $1"
-  if ! git submodule update --quiet "$1"
-  then
-      echo "** FAILED ** $1" && false
-  fi
+  git submodule update --quiet "$1"
+fi
+
+if [ $? != 0 ]
+then
+  echo " ** Failed ** $1" && false
 fi
