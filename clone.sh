bf7351d447220dbae506566c016f4fb6af8808e5
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-06-14 01:44:00 +0100

    Ignore errors on failed initial clones

--- clone.sh
+++ clone.sh
@@ -1,5 +1,5 @@
 echo "Cloning $1"
-if ! git submodule update --quiet --depth 1 "$1"
+if ! git submodule update --quiet --depth 1 "$1" 2> /dev/null
 then
   echo "Submodule out of date: $1"
   sh check.sh "$1"
