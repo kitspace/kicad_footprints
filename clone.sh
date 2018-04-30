2a70646d55cc5a60d7eb9b1b427a61d76c471606
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-04-30 22:15:06 +0100

    Stop init updating sub-modules

--- clone.sh
+++ clone.sh
@@ -2,5 +2,5 @@ echo "Initializing $1"
 if ! git submodule update --quiet --depth 1 "$1" 2> /dev/null
 then
   echo "Submodule out of date: $1"
-  sh check.sh "$1"
+  git submodule update --quiet "$1"
 fi
