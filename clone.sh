b1bbc56acc0e58ff021edcb2880ed7d4b554b1a9
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-04-30 22:57:31 +0100

    Adjust fetching of out of date sub modules

--- clone.sh
+++ clone.sh
@@ -2,5 +2,6 @@ echo "Initializing $1"
 if ! git submodule update --quiet --depth 1 "$1" 2> /dev/null
 then
   echo "Submodule out of date: $1"
-  git submodule update --quiet "$1"
+  pushd "$1" && git fetch --unshallow --quiet
+  popd && git submodule update --quiet "$1"
 fi
